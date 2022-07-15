138cfb6cbbab4b992c4d4c0d3c290060c9e0c0d6
vinicius barreto barbosa <vinicius.vbb98@gmail.com>
2022-07-15 09:17:03 -0300

    modelando todo o front-end

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css'
+import logo from '../../../assets/images/logo.png'
+function Header() {
+    return(
+        <header className="dsmeta-logo-container">
+        <div> <img src={logo} alt=""/>
+            <h1>DSmETA</h1>
+           
+            <a href="https://www.linkedin.com/in/vinicius-barreto-09986b220/"> <p>Desenvolvido por de vinicius.vbb98@gmail.com
+            </p></a></div>
+       
+    
+    
+    
+    
+    </header>
+  
+)
+  }
+  
+  export default Header
+  
---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,102 @@
+import{useState}from 'react'
+import DatePicker from "react-datepicker"   ;
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../NotificationButton";
+import "./styles.css";
+function SalesCard() {
+    const max = new Date();
+    const min =new Date(new Date().setDate(new Date().getDate() - 365));
+
+  const [minDate,setMinDate]=useState(min) ;
+  const [maxDate,setMaxDate]=useState(max);
+    return (
+    <div className="dsmeta-card">
+      <h2 className="desmeta-sales-titles">Vendas</h2>
+      <div>
+        <div className="desmeta-form-control-container">
+          <DatePicker
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className="desmeta-form-control-container">
+          <DatePicker
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+      </div>
+      <div>
+        <table className="dsmeta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">Id</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">15</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="dsmeta-red-btn-container">
+                  <div className="dsmeta-red-btn">
+                    <NotificationButton />
+                  </div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">15</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="dsmeta-red-btn-container">
+                  <div className="dsmeta-red-btn">
+                    <NotificationButton />
+                  </div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">15</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="dsmeta-red-btn-container">
+                  <div className="dsmeta-red-btn">
+                    <NotificationButton />
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default SalesCard;
